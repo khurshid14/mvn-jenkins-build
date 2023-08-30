e7f4fb49d5ce95420023800a4fc86bdf52b10fa2
shahbaz ahmad <shahbaz Ahmad>
2023-08-30 15:10:49 +0530

    Build

---
+++ src/main/java/automationpkg/Calculator.java
@@ -0,0 +1,37 @@
+package automationpkg;
+
+public class Calculator 
+{
+	
+	public int sum(int a, int b)
+	{
+		return (a + b);
+	}
+	public int diff(int a, int b)
+	{
+		return (a - b);
+	}
+	public int mul(int a, int b)
+	{
+		return (a * b);
+	}
+	public int div(int a, int b)
+	{
+		return (a / b);
+	}
+	public void display()
+	{
+		System.out.println("WELCOME HOME:");
+	}
+	
+	
+	public static void main(String[] args) 
+	{
+		Calculator obj = new Calculator();
+		System.out.println("SUM = " +obj.sum(10, 2));
+		System.out.println("DIFF = " +obj.diff(10, 2));
+		System.out.println("MUL = " +obj.mul(10, 2));
+		System.out.println("DIV = " +obj.div(10, 2));
+		obj.display();
+	}
+}
