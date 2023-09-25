c099756f56da47d90357c354f9afad4bf2eaac98
Shahbaz Ahmad <shahbaz Ahmad>
2023-09-25 23:20:17 +0530

    sonarqube dependencies and profile added

---
+++ src/main/java/automationpkg/TestSonar.java
@@ -0,0 +1,19 @@
+package automationpkg;
+
+public class TestSonar 
+{
+	public int sum(int a, int b)
+	{
+		int c = 8 + 8;
+		int d;
+		return c;
+	}
+	
+
+	public static void main(String[] args) 
+	{
+		TestSonar obj = new TestSonar();
+		System.out.println("sum is :" +obj.sum(10, 5));	
+	}
+
+}
